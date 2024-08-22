17e8e451145565ffeaad86f10b5620db927f1ac1
kmvishnu <kmvishnu1996@gmail.com>
2024-08-22 18:27:40 +0530

    RefreshToken is added . Healthcheck Route is added

--- backend/src/Controllers/loginController.js
+++ backend/src/Controllers/loginController.js
@@ -1,7 +1,10 @@
-const bcrypt = require('bcryptjs'); 
-const jwt = require('jsonwebtoken');
-const { saveRefreshToken } = require("../Common/redisClient"); 
-const { verifyUser, createUser } = require("../Components/UserComponent"); 
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const {
+  saveRefreshToken,
+  verifyRefreshToken,
+} = require("../Common/redisClient");
+const { verifyUser, createUser } = require("../Components/UserComponent");
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -13,7 +16,7 @@ const login = async (req, res) => {
       return res.status(401).json({ error: "Invalid email or password" });
     }
 
-    const hashedPassword = user ? user.password : '';
+    const hashedPassword = user ? user.password : "";
 
     const passwordMatch = await bcrypt.compare(password, hashedPassword);
 
@@ -35,15 +38,26 @@ const login = async (req, res) => {
       time: Date(),
       name: user.name,
       id: user._id,
-      exp: Math.floor(Date.now() / 1000) + Number(process.env.REFRESH_TOKEN_EXPIRESIN),
+      exp:
+        Math.floor(Date.now() / 1000) +
+        Number(process.env.REFRESH_TOKEN_EXPIRESIN),
     };
 
     const token = jwt.sign(data, jwtSecretKey);
     const refreshToken = jwt.sign(refreshData, jwtRefreshKey);
 
-    await saveRefreshToken(refreshToken, user._id, Number(process.env.REFRESH_TOKEN_EXPIRESIN));
+    await saveRefreshToken(
+      refreshToken,
+      user._id,
+      Number(process.env.REFRESH_TOKEN_EXPIRESIN)
+    );
 
-    res.status(200).json({ status: "success", token, refreshToken, name: user ? user.name : '' });
+    res.status(200).json({
+      status: "success",
+      token,
+      refreshToken,
+      name: user ? user.name : "",
+    });
   } catch (error) {
     console.error("Error during login:", error);
     return res.status(500).json({ error: "Internal server error" });
@@ -54,30 +68,73 @@ const signUp = async (req, res) => {
   const { email, password, name } = req.body;
 
   if (!name || name.length < 2 || name.length > 20) {
-    return res.status(400).json({ status: "error", message: "Name must be between 2 and 20 characters long." });
+    return res.status(400).json({
+      status: "error",
+      message: "Name must be between 2 and 20 characters long.",
+    });
   }
 
   if (!password || password.length < 4 || password.length > 20) {
-    return res.status(400).json({ status: "error", message: "Password must be between 4 and 20 characters long." });
+    return res.status(400).json({
+      status: "error",
+      message: "Password must be between 4 and 20 characters long.",
+    });
   }
 
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!email || !emailRegex.test(email)) {
-    return res.status(400).json({ status: "error", message: "Please provide a valid email address." });
+    return res.status(400).json({
+      status: "error",
+      message: "Please provide a valid email address.",
+    });
   }
 
   try {
     const result = await createUser(name, email, password);
     if (result) {
-      return res.status(200).json({ status: "success", message: "User Created" });
+      return res
+        .status(200)
+        .json({ status: "success", message: "User Created" });
     } else {
-      return res.status(500).json({ status: "error", message: "Failed to create user." });
+      return res
+        .status(500)
+        .json({ status: "error", message: "Failed to create user." });
     }
   } catch (error) {
     console.error("Error during sign up:", error);
-    return res.status(500).json({ status: "error", message: "Internal server error" });
+    return res
+      .status(500)
+      .json({ status: "error", message: "Internal server error" });
   }
 };
 
+const refreshToken = async (req, res) => {
+  const { refreshToken } = req.body;
+  const jwtSecretKey = process.env.JWT_SECRET_KEY;
+  const jwtRefreshKey = process.env.JWT_REFRESH_KEY;
+  if (!refreshToken) return res.status(401);
 
-module.exports = { login, signUp };
+  const userId = await verifyRefreshToken(refreshToken);
+  if (!userId) return res.status(403);
+
+  jwt.verify(refreshToken, jwtRefreshKey, async (err, user) => {
+    if (err || !user) return res.status(403);
+
+    const data = {
+      time: Date(),
+      name: user.name,
+      id: user.id,
+      exp: Math.floor(Date.now() / 1000) + Number(process.env.EXPIRESIN),
+    };
+
+    const token = jwt.sign(data, jwtSecretKey);
+
+    res.json({ token });
+  });
+};
+
+const healthCheck = (req, res) => {
+  return res.json("BlogosHub Server is up and running");
+};
+
+module.exports = { login, signUp, refreshToken, healthCheck };
--- backend/src/Routes/Routes.js
+++ backend/src/Routes/Routes.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const { login, signUp } = require("../Controllers/loginController"); 
+const { login, signUp, healthCheck, refreshToken } = require("../Controllers/loginController"); 
 const { addBlog, viewAllBlogs, viewAllPrivateBlogs, deleteBlog, editTodo } = require("../Controllers/blogController");
 const { validateToken } = require("../Middlewares/validateToken");
 
 const Routes = express.Router();
 
 Routes.get("/viewAllBlogs", viewAllBlogs);
+Routes.get("/test",healthCheck)
 
 Routes.post("/login", login);
 Routes.post("/register", signUp);
+Routes.post("/refreshToken",refreshToken)
 
 Routes.post("/createBlog", [validateToken], addBlog);
 Routes.delete("/deleteBlog/:id", [validateToken], deleteBlog);
